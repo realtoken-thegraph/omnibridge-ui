2f38a3413ec27efd7f25502621dfda932f92a0d2
Bastien Silhol <bastien.silhol@epitech.eu>
2023-12-15 14:11:12 +0100

    feat: rewrote packagejson removed constants from tree

--- src/mappings/constants.ts
+++
@@ -1,5 +0,0 @@
-import { Address } from "@graphprotocol/graph-ts"
-
-export const mediator = "0x4ad100a0c602f27fa3ecdaee8c58e9427d6a2a9b"
-
-export const mediatorAddress = Address.fromString("0x4ad100a0c602f27fa3ecdaee8c58e9427d6a2a9b");
